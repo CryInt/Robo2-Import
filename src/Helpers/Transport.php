f9e20af2e98c2cf023608a6080c649e2e228169e
Cry <cry.int@gmail.com>
2026-01-19 13:20:11 +0100

    v4.1.0 - query via curl package

--- src/Helpers/Transport.php
+++ src/Helpers/Transport.php
@@ -1,6 +1,7 @@
 <?php
 namespace Robo2Import\Helpers;
 
+use CryCMS\CURL\CURL;
 use Robo2Import\Interfaces\ConfigInterface;
 
 class Transport
@@ -12,37 +13,28 @@ class Transport
         $this->config = $config;
     }
 
-    public function cUrl($params = [], $post = [])
+    public function cUrl($params = [], $post = []): ?string
     {
-        $ch = curl_init();
+        $location = $this->config->host . '/api/?' . http_build_query($params);
 
-        $params['api'] = $this->config->version;
-
-        curl_setopt($ch, CURLOPT_URL, $this->config->host . '/api/?' . http_build_query($params));
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 480);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 480);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Login: ' . $this->config->prefix,
-            'Auth: '.md5($this->config->prefix . '+' . $this->config->apiKey)
-        ]);
-
-        if (!empty($post) && is_array($post) && count($post) > 0) {
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        if (!empty($post)) {
+            $query = CURL::post($location)->data($post);
+        }
+        else {
+            $query = CURL::get($location);
         }
 
-        $data = curl_exec($ch);
+        $query = $query->timeout(480)
+            ->connectTimeout(480)
+            ->ssl(false)
+            ->header('Login', $this->config->prefix)
+            ->header('Auth', md5($this->config->prefix . '+' . $this->config->apiKey));
 
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $response = $query->send();
+        if ($response->isSuccess) {
+            return $response->body;
+        }
 
-        curl_close($ch);
-
-        return ($httpCode >= 200 && $httpCode < 300) ? $data : false;
+        return null;
     }
 }
